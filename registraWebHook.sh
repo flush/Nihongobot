c4dffb0830be94041b0cc82866e16d3606beb372
flushaas <flushaas@gmail.com>
2021-10-06 22:59:02 +0000

    cambiado el dominio a flushaas.es

--- registraWebHook.sh
+++ registraWebHook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #https://infallible-chebyshev.82-223-98-235.plesk.page/
-curl -F "url=https://infallible-chebyshev.82-223-98-235.plesk.page:443/Nihongobot" https://api.telegram.org/bot1857814526:AAE8SpbHkklx-RJiwqtT5I2MiVEDdeNYWVU/setWebhook
+curl -F "url=https://flushaas.es/Nihongobot/Nihongobot" https://api.telegram.org/bot1857814526:AAE8SpbHkklx-RJiwqtT5I2MiVEDdeNYWVU/setWebhook
